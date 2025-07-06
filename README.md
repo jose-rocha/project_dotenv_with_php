b1aa1b9fc959d6cbc5d07fbfe1187fbacf18b605
José Rocha <josemarlerocha@hotmail.com>
2025-07-05 23:36:21 -0300

    Adicionado o README.md e uns TODOS

---
+++ README.md
@@ -0,0 +1,17 @@
+# PHP Project project_dotenv_with_php
+
+A PHP Project
+
+## Install the dependencies
+
+```bash
+composer install
+```
+
+### Create file .env based in .env.example
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+```bash
+php -S 0.0.0.0:8001 -t src/public/
+```
--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,18 @@
 <?php
 
+// ====================================================================
+// TODO:  Não esquecer de criar um arquivo .env se não ao executar irá gerar um erro
+// ====================================================================
+
+// FIXME: Verificar se o caminho do autoload está correto
+// NOTE: Este projeto usa vlucas/phpdotenv para gerenciar variáveis de ambiente
+// WARNING: Sem o arquivo .env, a aplicação irá falhar
+
 require_once '../../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 // var_dump($dotenv);
-var_dump($_ENV['USER']);
+// var_dump($_ENV['USER']);
+var_dump($_ENV);
